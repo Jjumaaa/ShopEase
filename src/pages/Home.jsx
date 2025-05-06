5a0aaad7ad66f638cbca2a7bb5c3def2d4cfdbd0
Juma <gerald.juma@student.moringaschool.com>
2025-05-06 19:06:21 +0300

    removed unecessaries

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,16 +11,7 @@ const Home = () => {
         <Link to="/products" className="shop-now-btn">Shop Now</Link>
       </section>
       
-      <section className="featured">
-        <h2>Featured Products</h2>
-        <div className="featured-products">
-          <div className="featured-item">
-            <img src="https://images.unsplash.com/photo-1723961617032-ef69c454cb31?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHByZW1pdW0lMjBoZWFkcGhvbmVzfGVufDB8fDB8fHww" alt="Featured Product" />
-            <h3>Premium Headphones</h3>
-            <p>$129.99</p>
-          </div>
-        </div>
-      </section>
+    
     </div>
   );
 };
